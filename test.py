6abd0bc3227cd9bca617619b4cd4fe74290ae9a4
tsbabu <tsbabu108@gmail.com>
2021-04-01 11:34:16 +0530

    feffe

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 print('babu')
 print('Thimmisetty Surendra babu')
 print('i was worked in mlinx solutions banglore')
+print('babu')
+print('Thimmisetty Surendra babu')
+print('i was worked in mlinx solutions banglore')
+
